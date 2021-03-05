552665fcc375165d074af2a6e65ba50f9ac438e9
JoseMarioAriasAcevedo <78364618+JoseMarioAriasAcevedo@users.noreply.github.com>
2021-03-05 18:27:53 -0500

    Update Cliente.java

--- Taller_RMI/src/Cliente/Cliente.java
+++ Taller_RMI/src/Cliente/Cliente.java
@@ -6,6 +6,7 @@ import java.rmi.*;
 
 public class Cliente {
 	public static void main(String args[]) {
+		//Las palabras asignadas son hola y mundo
 		String a = "hola";
 		String b = "mundo";
 		String buffer = null;
@@ -14,7 +15,7 @@ public class Cliente {
 			System.out.println("Buscando Objeto ");
 			
 			Interfaz micomparador = (Interfaz) Naming.lookup("rmi://"+ args[0]+"/" + "MiComparador");
-			
+			//Imprime los resultados de cada metodo
 			System.out.println(micomparador.comparaCadenas(a, b));
 			System.out.println(micomparador.concatenCadenas(a, b, buffer));
 			System.out.println(micomparador.copiaCadena(a, buffer));
