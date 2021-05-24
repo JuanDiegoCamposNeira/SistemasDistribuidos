6ea6a154ce0e742436eb6614e1462ddd62234aab
Juan Diego Campos Neira <53028714+JuanDiegoCamposNeira@users.noreply.github.com>
2021-05-24 15:19:27 -0500

    Correciones

--- ProyectoFinal/BackupReplica.py
+++ ProyectoFinal/BackupReplica.py
@@ -65,30 +65,12 @@ register_socket = context.socket( zmq.REQ )
 register_socket.connect( 'tcp://25.0.228.65:6000' )
 
 # Read the initial state of the DataBase
-# Open file
 data_base = open('BaseDeDatos.txt', 'r')
-# Read file 
-line = 1
 for single_book_info in data_base: 
-    # Don't read the first line
-    if line == 1: 
-        line += 1
-        continue
-    else: 
-        line += 1
-
     # Get book parameters 
     id_libro, id_ejemplar, nombre, autor, estado, fecha_prestamo, fecha_devolucion, sede_prestamo = single_book_info.split(',')
-    # Create dates if book available
-    loan_date = fecha_prestamo 
-    return_date = fecha_devolucion
-    if estado == 'prestado': 
-        fecha_prestamo = fecha_prestamo.split('-')
-        loan_date = datetime.datetime(int(fecha_prestamo[0]), int(fecha_prestamo[1]), int(fecha_prestamo[2]))
-        fecha_devolucion = fecha_devolucion.split('-')
-        return_date = datetime.datetime(int(fecha_devolucion[0]), int(fecha_devolucion[1]), int(fecha_devolucion[2]))
     # Save book
-    library.append( Libro(id_libro, id_ejemplar, nombre, autor, estado, loan_date, return_date, sede_prestamo) )
+    library.append( Libro(id_libro, id_ejemplar, nombre, autor, estado, fecha_prestamo, fecha_devolucion, sede_prestamo) )
 
     # If book isn't available, continue
     if estado == 'prestado': 
@@ -108,6 +90,7 @@ def handle_database_modification(operation: str, book: str, branch: str):
     # If is a return operation
     if operation == 'devolucion':
         # Search book 
+        book_found = False
         for current_book in library: 
             if (current_book.nombre == book or current_book.id_libro) and current_book.estado == 'prestado': 
                 print(current_book)
@@ -115,44 +98,64 @@ def handle_database_modification(operation: str, book: str, branch: str):
                 current_book.estado = 'disponible'
                 current_book.fecha_prestamo = '_'
                 current_book.fecha_devolucion = '_'
-                current_book.sede = '_'
+                current_book.sede_prestamo = '_\n'
                 print(current_book)
                 # Add one to available books
                 ejemplares_disponibles[current_book.id] += 1  
+                book_found = True
                 break
+        if not book_found: 
+            print('Solicitud de devolución, libro no encontrado')
+            return 'Solicitud de devolución, libro no encontrado' 
                 
     # If is a renew operation 
     elif operation == 'renovacion':
         # Search book 
+        book_found = False 
         for current_book in library: 
             if (current_book.nombre == book or current_book.id_libro) and current_book.estado == 'prestado': 
-                print(current_book)
                 # Modify current book 
-                fecha_devolucion = current_book.fecha_devolucion
-                fecha_devolucion = datetime.datetime.strptime(fecha_devolucion, '%y/%m/%d') + datetime.timedelta(days=7)
-                current_book.fecha_devolucion = fecha_devolucion
-                print(current_book)
+                year, month, day = current_book.fecha_devolucion.split('-')
+                fecha_devolucion = datetime.date( int(year), int(month), int(day) ).strftime('%y-%m-%d')
+                fecha_devolucion = datetime.datetime.strptime(fecha_devolucion, '%y-%m-%d') + datetime.timedelta(days=7)
+                current_book.fecha_devolucion = fecha_devolucion.strftime('%y-%m-%d')
+                book_found = True
                 break
+        if not book_found: 
+            print('Renovación de préstamo, libro no encontrado')
+            return 'Renovación de préstamo, libro no encontrado' 
 
     # If is a request operation 
     elif operation == 'solicitud':
         # Search book 
-        for current_book in library: 
-            if current_book.nombre == book or current_book.id_libro:
+        book_found = False 
+        for i in range(0, len(library)): 
+            if (library[ i ].nombre == book or library[ i ].id_libro == book) and library[ i ].estado == 'disponible':
+                print('Libro encontrado', library[i].nombre, library[i].id_ejemplar)
+                print('Ejemplares : ', ejemplares_disponibles[library[i].id_libro])
                 # Validate available books
-                if ejemplares_disponibles[current_book.id] >= 1: 
-                    ejemplares_disponibles[current_book] -= 1
-                    current_book.estado = 'prestado'
-                    current_book.fecha_prestamo = datetime.datetime.today().strftime('%y%m%d')
-                    current_book.fecha_devolucion = datetime.datetime.strptime(current_book.fecha_prestamo, '%y/%m/%d') + datetime.timedelta(days=7) 
-                    current_book.sede = branch
+                if ejemplares_disponibles[library[ i ].id_libro] >= 1: 
+                    ejemplares_disponibles[library[ i ].id_libro] -= 1
+                    library[ i ].estado = 'prestado'
+                    library[ i ].fecha_prestamo = datetime.datetime.today().strftime('%y-%m-%d')
+                    return_date = datetime.datetime.strptime(datetime.datetime.today().strftime('%y-%m-%d'), '%y-%m-%d') + datetime.timedelta(days=7)
+                    library[ i ].fecha_devolucion = return_date.strftime('%y-%m-%d')
+                    library[ i ].sede_prestamo = branch + '\n'
+                    book_found = True
+                    break
                 else: 
+                    print('No hay ejemplares disponibles')
+                    book_found = True
                     return 'No hay ejemplares disponibles para realizar el prestamo'
+
+        if not book_found: 
+            print('Solicitud de préstamo, libro no encontrado')
+            return 'Solicitud de préstamo, libro no encontrado'  
 
 
     # Override file with new data 
     data_base = open('BaseDeDatos.txt', 'w')
-    data_base.write('# Id, Nombre_Libro, Autor, Ejemplares_Disponibles \n')
+    # data_base.write('# Id, Nombre_Libro, Autor, Ejemplares_Disponibles \n')
     for single_book_info in library: 
         id_libro = single_book_info.id_libro
         id_ejemplar = single_book_info.id_ejemplar
@@ -160,9 +163,8 @@ def handle_database_modification(operation: str, book: str, branch: str):
         autor = single_book_info.autor
         estado = single_book_info.estado
         fecha_prestamo = single_book_info.fecha_prestamo 
-        fecha_devolucion = single_book_info.fecha_prestamo  
+        fecha_devolucion = single_book_info.fecha_devolucion  
         sede_prestamo = single_book_info.sede_prestamo  
-        
         # Write book
         new_book = id_libro + ',' + id_ejemplar + ',' + nombre + ',' + autor + ',' + estado + ',' + str(fecha_prestamo) + ',' + str(fecha_devolucion) + ',' + sede_prestamo
         data_base.write( new_book ) 
